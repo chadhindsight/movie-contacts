64eae85fedf8659aabc32b84b5e3296a03713a71
chad hinds <chadrickj8@gmail.com>
2023-12-07 12:28:58 -0500

    change title text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Big Buns</h1>
+      <h1 className='text-3xl font-bold underline'>Movie Contacts</h1>
     </>
   )
 }
