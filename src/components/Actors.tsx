130d19bee6bcfd6bc99d9a1b314893212766e17d
chad hinds <chadrickj8@gmail.com>
2023-12-08 19:43:34 -0500

    Initial setup of actors component

--- src/components/Actors.tsx
+++ src/components/Actors.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
 const Actors = () => {
-    // Loop through actors in this component
     return (
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" className="px-6 py-3">
-                    Actor
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Popularity
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
-                </th>
-            </tr>
-        </thead>
+        <table>
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="px-6 py-3">
+                        Actor
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Popularity
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* Actor component will be rendered here */}
+            </tbody>
+        </table>
     );
 };
 
