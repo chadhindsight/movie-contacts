3df263d4117e1817f2c62f7309e10840e6d234a2
chad hinds <chadrickj8@gmail.com>
2023-12-10 11:10:58 -0500

    Add deleteActor to the related interface

--- src/components/Actor.tsx
+++ src/components/Actor.tsx
@@ -4,9 +4,10 @@ export interface ActorInterface {
     pictureUrl: string;
     popularity: number;
     id: string;
+    deleteActor?: () => void;
 }
 
-const Actor: React.FC<ActorInterface> = ({ name, pictureUrl, popularity, id }) => {
+const Actor: React.FC<ActorInterface> = ({ name, pictureUrl, popularity, id, deleteActor }) => {
     return (
         <tr data-id={id}>
             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -17,7 +18,8 @@ const Actor: React.FC<ActorInterface> = ({ name, pictureUrl, popularity, id }) =
                 {popularity}
             </td>
             <td className="px-6 py-4">
-                <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                    onClick={deleteActor}>
                     Delete
                 </button>
             </td>
