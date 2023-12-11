4cb0fa2e4b9cbb432ebd5df856a2835103b4e154
chad hinds <chadrickj8@gmail.com>
2023-12-11 12:16:48 -0500

    Abort testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import { ContactsContext } from './main'
 import Actors from './components/Actors'
-import Header from './components/Header/Header'
+import Header from './components/Header'
 
 function App() {
 
