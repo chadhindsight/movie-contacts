d5fc8e472e97c3a367ce8768cc1a997b9587a1ae
chad hinds <chadrickj8@gmail.com>
2023-12-19 15:59:02 -0500

    Initial imports and setup

---
+++ src/tests/Actor.test.tsx
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import Actor from '../components/Actor';
+
+
+describe('Actors component', () => {
+
+}
