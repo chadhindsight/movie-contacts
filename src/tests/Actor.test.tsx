c9c4851df1e57e0bc4d686157cb63f5fb0e52926
chad hinds <chadrickj8@gmail.com>
2023-12-20 12:17:36 -0500

    Add mock actor data

--- src/tests/Actor.test.tsx
+++ src/tests/Actor.test.tsx
@@ -4,7 +4,14 @@ import Actor from '../components/Actor';
 
 
 describe('Actors component', () => {
-    it('renders a component that displays info for an actor', () => {
+    // mock actor info 
+    const idrisTest = {
+        "name": "Idris Elba",
+        "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
+        "popularity": 11.622713,
+        "id": "11731993-0604-4bee-80d5-67ad845d0a38"
+    }
 
+    it('renders a component that displays info for an actor', () => {
     })
-}
+})
